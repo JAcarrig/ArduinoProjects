427380b84994167fc151e4aeccdb12f6abcf38e6
Jack <jxc2476@g.rit.edu>
2020-02-16 19:32:58 -0500

    project 1 led function setup

--- Project1/Project1.ino
+++ Project1/Project1.ino
@@ -1,16 +1,22 @@
+#include <Adafruit_NeoPixel.h>
 
 
+#define LED_PIN 2
+#define LED_COUNT 3
 const int input = A2;
 int potValue = 0;
 int potOutputValue = 0;
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2,OUTPUT);
-  digitalWrite(2, HIGH);
-
-
+  //pinMode(2,OUTPUT);
+  //digitalWrite(2, HIGH);
   Serial.begin(9600);
+
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
   
 }
 
@@ -26,5 +32,15 @@ void loop() {
   Serial.print(" -> ");
   Serial.print(potOutputValue);
 
+  uint32_t color;
+  strip.clear();
+  //set color based on POT value
+  color = strip.Color(potOutputValue,0,0);
+  //output to color to neopixel strip
+  strip.setPixelColor(0, color);
+
+  strip.show();
   delay(2);
+
+  
 }
